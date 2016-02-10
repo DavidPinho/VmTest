1871410e188bf77e1004c273da3ed9a73bfaed75
David Pinho <davidpinho08@gmail.com>
2016-02-10 18:46:04 -0300

    Create Casarao.java

---
+++ test/pkgtest/Casarao.java
@@ -0,0 +1,15 @@
+package pkgtest;
+
+public class Casarao {
+
+	private int quartos;
+	
+	public void setQuartos(int q) {
+		this.quartos = q;
+	}
+	
+	public int getQuartos(){
+		return this.quartos;
+	}
+	
+}
